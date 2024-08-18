ba3efb36830a75da68c8c3b7d0dc443578336589
AlessanderMorales <alessmoralesmo@gmail.com>
2024-08-18 00:03:10 -0400

    Saludar en idioma

--- src/saludos.js
+++ src/saludos.js
@@ -1,19 +1,35 @@
+// app.js
 
 document.getElementById('saludarBtn').addEventListener('click', function() {
+    const idioma = prompt('¿En qué idioma quieres que te salude? (ES/EN)');
     const nombre = prompt('¿Cuál es tu nombre?');
     const genero = prompt('¿Cuál es tu género? (M/F)');
     const edad = prompt('¿Cuál es tu edad?');
     let saludo;
 
-    if (genero.toUpperCase() === 'M') {
-        saludo = `Hola, señor ${nombre}`;
-    } else if (genero.toUpperCase() === 'F') {
-        saludo = `Hola, señora ${nombre}`;
-    } else {
-        saludo = `Hola, ${nombre}`;
-    }
+    if (idioma.toUpperCase() === 'ES') {
+        if (genero.toUpperCase() === 'M') {
+            saludo = `Hola, señor ${nombre}`;
+        } else if (genero.toUpperCase() === 'F') {
+            saludo = `Hola, señora ${nombre}`;
+        } else {
+            saludo = `Hola, ${nombre}`;
+        }
 
-    saludo += `, tienes ${edad} años.`;
+        saludo += `, tienes ${edad} años.`;
+    } else if (idioma.toUpperCase() === 'EN') {
+        if (genero.toUpperCase() === 'M') {
+            saludo = `Hello, Mr. ${nombre}`;
+        } else if (genero.toUpperCase() === 'F') {
+            saludo = `Hello, Ms. ${nombre}`;
+        } else {
+            saludo = `Hello, ${nombre}`;
+        }
+
+        saludo += `, you are ${edad} years old.`;
+    } else {
+        saludo = 'Idioma no reconocido';
+    }
 
     document.getElementById('mensaje').textContent = saludo;
 });
